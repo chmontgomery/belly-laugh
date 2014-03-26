e7992f3c8b3edeb769451996875f5a6d1005ca94
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-03-26 10:30:08 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+  exec = require('exec'),
+  app = express();
+
+function playLaugh(cb) {
+  exec('afplay belly_laugh.mp3', cb);
+}
+
+app.get('/', function(req, res){
+  playLaugh(function() {
+    res.send('belly laugh <input type="button" onclick="window.location.reload();" value="reload"/>');
+  });
+});
+
+app.get('/bellylaugh', function(req, res){
+  playLaugh(function() {
+    res.send(200);
+  });
+});
+
+app.listen(7881);
+console.log('server listening at http://localhost:7881');
